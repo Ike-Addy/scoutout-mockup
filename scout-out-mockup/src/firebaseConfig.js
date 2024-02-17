cba81ba83469a5847e3e9ff575578f57bf0019a4
Ike Addy <ikaddy05@gmail.com>
2024-02-17 18:21:31 -0500

    Added firebase && added exported app in firebaseconfig file

---
+++ scout-out-mockup/src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaC3_I-Y1Xy2_AAiOCJ7KPXXeNeyIHyxU",
+  authDomain: "scout-out-mockup.firebaseapp.com",
+  projectId: "scout-out-mockup",
+  storageBucket: "scout-out-mockup.appspot.com",
+  messagingSenderId: "142352770444",
+  appId: "1:142352770444:web:20ca510c74c3e3a11fe2a9",
+  measurementId: "G-1HHX8M6PPK"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
